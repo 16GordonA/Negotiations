c9cace9c1aec485a8dac384e8a8f19792bdec239
A G <16gordona@my.weston.org>
2017-06-25 11:30:43 -0400

    Click and Drag

--- Negotiation.py
+++ Negotiation.py
@@ -1,1 +1,57 @@
-#hashtag
+import pygame, sys, random, time, pygame.mixer, pygame.font
+from pygame.locals import *
+from pygame.font import *
+
+from Resources import *
+
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 450
+
+size = SCREEN_WIDTH, SCREEN_HEIGHT
+screen = pygame.display.set_mode(size)
+
+
+background = pygame.image.load('background.png')
+mine = pygame.image.load('Mine.png')
+
+mtest = Resource(mine, 'A', 100, 100)
+
+time = 0
+
+while True:
+    screen.blit(background, (0, 0))
+    all_rss.draw(screen)
+    
+    for event in pygame.event.get():
+        
+        if event.type == pygame.QUIT:
+            sys.exit()
+            
+        if event.type == MOUSEBUTTONDOWN:
+            for rsc in  all_rss:
+                print(pygame.mouse.get_pos())
+                print(rsc.image.get_rect())
+                if rsc.rect.collidepoint(pygame.mouse.get_pos()):
+                    rsc.selected = True;
+                else:
+                    rsc.selected = False;
+        else:
+            for rsc in all_rss:
+                if rsc.selected:
+                    if rsc.rect.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]:
+                        rsc.updatePos(pygame.mouse.get_pos())
+                    else:
+                        rsc.selected = False
+    
+    key = pygame.key.get_pressed()
+    
+    for rsc in all_rss:
+        rsc.update();
+    
+    if key[K_ESCAPE]:
+        sys.exit()
+            
+        
+    pygame.display.update()
+    pygame.event.pump()
+        
--- Resources.py
+++ Resources.py
@@ -1,1 +1,25 @@
-import pygame.*
+import pygame, sys
+from pygame.locals import *
+all_rss = pygame.sprite.Group()
+
+
+class Resource(pygame.sprite.Sprite):
+    def __init__(self, image, type, startX, startY):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image.convert_alpha()  # transparent image
+        self.rect = self.image.get_rect().move(startX, startY)  # rect is for blitting
+        self.selected = False
+        pygame.sprite.Sprite.__init__(self, all_rss)
+    
+    def update(self):
+        if self.selected:
+            self.image.set_alpha(0)
+        else:
+            self.image.set_alpha(255)
+        
+        #print(self.image.get_alpha())
+        
+    def updatePos(self, loc):
+        if self.selected:
+            self.rect = self.rect.move((-0.5*self.rect.width - self.rect.left,-0.5*self.rect.height - self.rect.top))
+            self.rect = self.rect.move(loc)
